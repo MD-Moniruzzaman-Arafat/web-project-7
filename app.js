d64c8a43ad54af8f5d6c41726b44a101c5b56af3
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-07-24 10:25:29 +0600

    calculate ellipse

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const triangleCalculate = document.getElementById('triangle');
 const rectangleCalculate = document.getElementById('rectangle');
 const parallelogramCalculate = document.getElementById('parallelogram');
 const rhombusCalculate = document.getElementById('rhombus');
+const pentagonCalculate = document.getElementById('pentagon');
+const ellipseCalculate = document.getElementById('ellipse');
 
 triangleCalculate.addEventListener('click', function () {
   // console.log('Triangle button clicked');
@@ -102,4 +104,54 @@ rhombusCalculate.addEventListener('click', function () {
   // clear input fields
   document.getElementById('rhombus-d1').value = '';
   document.getElementById('rhombus-d2').value = '';
+})
+
+pentagonCalculate.addEventListener('click', function () {
+  // console.log('Pentagon button clicked');
+  const p = document.getElementById('pentagon-p').value;
+  const b = document.getElementById('pentagon-b').value;
+  const area = 0.5 * (p * b);
+  // console.log('Pentagon Area:', area);
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <div class="text-[13px] flex justify-between items-center p-1">
+    <span>pentagon</span>
+    <span>${area}cm<sup>2</sup></span>
+    <button class="btn btn-xs btn-primary">Convert to m<sup>2</sup></button>
+  </div>
+`;
+
+  if (p > 0 || b > 0) {
+    document.getElementById('aria-list').appendChild(li)
+  }
+
+  // clear input fields
+  document.getElementById('pentagon-p').value = '';
+  document.getElementById('pentagon-b').value = '';
+})
+
+ellipseCalculate.addEventListener('click', function () {
+  // console.log('Ellipse button clicked');
+  const a = document.getElementById('ellipse-a').value;
+  const b = document.getElementById('ellipse-b').value;
+  const area = Math.PI * a * b;
+  // console.log('Ellipse Area:', area);
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+  <div class="text-[13px] flex justify-between items-center p-1">
+    <span>ellipse</span>
+    <span>${area.toFixed(2)}cm<sup>2</sup></span>
+    <button class="btn btn-xs btn-primary">Convert to m<sup>2</sup></button>
+  </div>
+`;
+
+  if (a > 0 || b > 0) {
+    document.getElementById('aria-list').appendChild(li)
+  }
+
+  // clear input fields
+  document.getElementById('ellipse-a').value = '';
+  document.getElementById('ellipse-b').value = '';
 })
